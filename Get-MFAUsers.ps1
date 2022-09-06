ff46e8102b1193569d55fdb9879eabd5d6d7ce0a
david-ruffin <76492205+david-ruffin@users.noreply.github.com>
2022-09-05 20:13:27 -0700

    Create Get-MFAUsers.ps1

---
+++ Get-MFAUsers.ps1
@@ -0,0 +1,21 @@
+# Get current list of users that dont have MFA enabled and store in variable
+$users = Import-Csv "c:\scripts\MFAStatus-Sep-05-2022.csv"
+# Get list of service account users that need to be removed from the final list
+$service_accounts = (Get-ADUser -Filter * -SearchBase 'DC=amtwoundcare,DC=com' | 
+    Where-Object { $_.DistinguishedName -like '*OU=Service Accounts,*' }).UserPrincipalName | sort
+# Create ArrayList because you're not able to remove items from Array
+[System.Collections.ArrayList]$demoArrayList= @()
+# Convert Array to Arraylist object
+$demoArrayList = $users
+# Check
+foreach ($i in 0..$service_accounts.count)
+    {
+    if ($service_accounts["$i"] -eq (($demoArrayList | where { $_.UserPrincipalName -eq $service_accounts["$i"] }).UserPrincipalName))
+        {
+            echo $service_accounts["$i"]
+            
+            $delete = $demoArrayList | where { $_.UserPrincipalName -eq $service_accounts["$i"] }
+            $demoArrayList.Remove($delete)
+        }
+    
+    } 

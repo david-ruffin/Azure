9c37fc653f850fec3e5549faf0a1c0b83eeb6480
david-ruffin <76492205+david-ruffin@users.noreply.github.com>
2022-10-07 08:27:19 -0700

    Update enable-psremoting.ps1

--- enable-psremoting.ps1
+++ enable-psremoting.ps1
@@ -1,4 +1,6 @@
 # Enable psremoting for domain joined computers
+# Set-Item WSMan:\localhost\Client\TrustedHosts -Value 'machineA,machineB'
 
 Enable-PSRemoting -Force -Confirm:$false
-Set-Item WSMan:localhost\client\trustedhosts -value AD.ruffin.com -Force
+Set-Item WSMan:localhost\client\trustedhosts -value 'AD.ruffin.com,rundeck.ruffin.com' -Force
+

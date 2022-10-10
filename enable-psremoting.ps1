55003b2b373ac42eb72728e03fcf0e7bf41d6ad0
david-ruffin <76492205+david-ruffin@users.noreply.github.com>
2022-10-10 10:54:22 -0700

    Update enable-psremoting.ps1

--- enable-psremoting.ps1
+++ enable-psremoting.ps1
@@ -4,3 +4,5 @@
 Enable-PSRemoting -Force -Confirm:$false
 Set-Item WSMan:localhost\client\trustedhosts -value 'AD.ruffin.com,rundeck.ruffin.com' -Force
 
+Enable-PSRemoting -Force -Confirm:$false -SkipNetworkProfileCheck
+Set-Item WSMan:localhost\client\trustedhosts -value 'ansible.amtwoundcare.com,amtprocess01.amtwoundcare.com,amtprocess02.amtwoundcare.com,rundeck.amtwoundcare.com' -Force 

0458f6847978a768e83004d289145f75b6a32cf9
david-ruffin <76492205+david-ruffin@users.noreply.github.com>
2022-10-13 14:57:12 -0700

    Update install-remote-server.ps1

--- install-remote-server.ps1
+++ install-remote-server.ps1
@@ -1,6 +1,14 @@
-$computerName = 'AMTWKSP-T01LDUM'
+# This script will install carbon black on AMT Workspace VM
+# Log into AWS
+
+# Get Workspace aname
+$computerName = Get-AWSWorkspace 'AMTWKSP-T01LDUM'
+# Ensure Workspace machine is online
+
+# Install carbon black
 $command = {
   Start-Process -Wait -FilePath 'C:\IT\installer_vista_win7_win8-64-3.8.0.684.msi' -ArgumentList '/qn /norestart COMPANY_CODE=TZ725OTDWDCT4NOSD6Z POLICY_NAME=AWS' -PassThru -WorkingDirectory 'C:\IT\'
   }
 $job = Invoke-Command -ComputerName $computerName -ScriptBlock $command -ErrorAction Stop -AsJob #-credential $Credentials1
 Wait-Job -Job $Job
+# Confirm agent installed correctly

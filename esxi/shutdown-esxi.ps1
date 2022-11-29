7ea67560feef2c3362bec0720d7eb2a271c9932a
Vyente David Ruffin <76492205+david-ruffin@users.noreply.github.com>
2022-11-28 22:36:25 -0800

    Update shutdown-esxi.ps1

--- esxi/shutdown-esxi.ps1
+++ esxi/shutdown-esxi.ps1
@@ -6,8 +6,8 @@ connect-viserver 192.168.30.5 -Credential $Credentials
 $vms = Get-VM | where {$_.PowerState -eq "PoweredOn"}
 $vms | foreach {Suspend-VM -VM $_ -Confirm:$false} 
 # Place the selected host into Maintenance Mode.
-Get-VMHost -Name PROBLEM_HOST | set-vmhost -State Maintenance
+Get-VMHost -Name '192.168.30.5' | set-vmhost -State Maintenance
  
 # Shutdown the host
-Stop-VMhost -VMhost PROBLEM_HOST -Confirm:$false
+Stop-VMhost -VMhost '192.168.30.5' -Confirm:$false
 Disconnect-viserver -Confirm:$false -force

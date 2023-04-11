44684eea0ac410fe1d925802bb76e2d5b1e545ce
Vyente David Ruffin <76492205+david-ruffin@users.noreply.github.com>
2023-04-11 09:59:19 -0700

    Create run-command-on-all-vms.ps1

---
+++ powershell/run-command-on-all-vms.ps1
@@ -0,0 +1,39 @@
+# Declare variables
+$vmcreds = get-credentials
+$viservers = '<vmware-server1>', '<vmware-server2>'
+ 
+# Log into vmware
+$viservers | ForEach-Object { connect-viserver -server $_ -credentials $vmcreds }
+ 
+# Define the list of VM names that are Windows and powered on
+$vmList = Get-VM | Where-Object { $_.PowerState -eq 'PoweredOn' -and $_.Guest.OSFullName -like 'Microsoft Windows*' }
+ 
+# Define the command you want to run
+$command = "net localgroup Administrators; hostname"
+ 
+# Create an empty array to store the results
+$results = @()
+ 
+# Loop through the list of VM names and run the command for each VM
+foreach ($vmName in $vmList) {
+    $scriptResult = Invoke-VMScript -VM $vmName -GuestCredential $vmcreds -ScriptText $command -ScriptType Powershell
+ 
+    # Process the output
+    $scriptOutput = $scriptResult.ScriptOutput -split "`r`n" | Where-Object { $_.Trim() -ne "" }
+    $hostname = $scriptOutput | Select-Object -Last 1
+    $adminMembers = ($scriptOutput | Select-Object -Skip 4 | Select-Object -SkipLast 2) -join ", "
+    $vmhost = (Get-VM $vmName | Get-VMHost).Name
+ 
+    # Create a custom object with the output information
+    $outputObject = [PSCustomObject]@{
+        VMName                 = $vmName
+        Hostname               = $hostname
+        LocalAdminGroupMembers = $adminMembers
+        VMHost                 = $vmhost
+    }
+     
+    # Add the output object to the results array
+    $results += $outputObject
+}
+# Display the output object
+$results 

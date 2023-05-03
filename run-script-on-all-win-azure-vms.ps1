5c05df5da5a96f689b71ec01c9bfa4a07303d83e
Vyente David Ruffin <76492205+david-ruffin@users.noreply.github.com>
2023-05-03 14:13:18 -0700

    Update run-script-on-all-win-azure-vms.ps1

--- run-script-on-all-win-azure-vms.ps1
+++ run-script-on-all-win-azure-vms.ps1
@@ -3,16 +3,16 @@ connect-azaccount
 $subs = (get-azsubscription).Name
 $results = $null
 $script = "C:\scripts\check-openssl-installed.ps1"
-$subs | foreach {
+$subs | ForEach-Object {
     set-azcontext -subscription $_ 
-    $azvms = get-azvm | where-object {$_.StorageProfile.OsDisk.OsType -eq "Windows"} 
-    $azvms | foreach {$status = Invoke-AzVmRunCommand `
-     -ResourceGroupName $_.ResourceGroupName `
-     -VMName $_.Name `
-     -CommandId "RunPowerShellScript" `
-     -ScriptPath $script -Verbose
-     # If theres response to script, store in $results variable
-     if ($null -ne $status.Value.message){$results += $status.Value.message}
-     }
-     }
-     $results
+    $azvms = get-azvm | where-object { $_.StorageProfile.OsDisk.OsType -eq "Windows" } 
+    $azvms | ForEach-Object { $status = Invoke-AzVmRunCommand `
+            -ResourceGroupName $_.ResourceGroupName `
+            -VMName $_.Name `
+            -CommandId "RunPowerShellScript" `
+            -ScriptPath $script -Verbose
+        # If theres response to script, store in $results variable
+        if ($null -ne $status.Value.message) { $results += $status.Value.message }
+    }
+}
+$results

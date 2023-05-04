b3daab62b3d316a2b8ad78ce9c70437e5371cc34
Vyente David Ruffin <76492205+david-ruffin@users.noreply.github.com>
2023-05-03 19:03:11 -0700

    Update run-command-on-all-vms.ps1

--- powershell/run-command-on-all-vms.ps1
+++ powershell/run-command-on-all-vms.ps1
@@ -1,9 +1,12 @@
 # Declare variables
-$vmcreds = get-credentials
+$Username = 'user'
+$Password = ConvertTo-SecureString 'password' -AsPlainText -Force
+$vmcreds = New-Object System.Management.Automation.PSCredential $Username, $Password
+ 
 $viservers = '<vmware-server1>', '<vmware-server2>'
  
 # Log into vmware
-$viservers | ForEach-Object { connect-viserver -server $_ -credentials $vmcreds }
+$viservers | ForEach-Object { connect-viserver -server $_ -credential $vmcreds }
  
 # Define the list of VM names that are Windows and powered on
 $vmList = Get-VM | Where-Object { $_.PowerState -eq 'PoweredOn' -and $_.Guest.OSFullName -like 'Microsoft Windows*' }

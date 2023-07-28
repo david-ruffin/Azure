fe4a1fd9e6e6c11f09280c5f7fa16070c294dcfd
Vyente David Ruffin <76492205+david-ruffin@users.noreply.github.com>
2023-07-28 10:04:05 -0700

    Update create-download-link.ps1

--- create-download-link.ps1
+++ create-download-link.ps1
@@ -1,19 +1,24 @@
 # Declare Vars
 # Connect to Azure using AZ module
 
-$ResourceGroupName = "resourcegroup"
-$StorageAccountName = "storage3545awdawdawd345345345345"
-$ContainerName = "yourcontainername"
-$LocalFilePath = "C:\OneDriveSetup.exe"
+$ResourceGroupName = "rg-prod-westus-aap"
+$StorageAccountName = "saansible454e5e4"
+$ContainerName = "ansible-automation-platform-aap"
+$LocalFilePath = "/Users/tech/Downloads/ansible-automation-platform-setup-bundle-2.4-1-x86_64.tar.gz"
 $fileName = Split-Path -Path $LocalFilePath -Leaf
 
+# Check if the resource group exists
+$resourceGroup = Get-AzResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue
 
-
+# If the resource group doesn't exist, create it
+if (-not $resourceGroup) {
+    New-AzResourceGroup -Name $ResourceGroupName -Location "West US"
+}
 
 # Create Storage Account if it doesn't exist
 $storageAccount = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -ErrorAction SilentlyContinue
 if (!$storageAccount) {
-    $storageAccount = New-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -Location "East US" -SkuName Standard_LRS -Kind StorageV2
+    $storageAccount = New-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -Location "West US" -SkuName Standard_LRS -Kind StorageV2
 }
 
 # Set Container's public access level to 'Blob'
@@ -21,7 +26,6 @@ $ctx = $storageAccount.Context
 New-AzStorageContainer -Name $ContainerName -Context $ctx -Permission Blob -ErrorAction SilentlyContinue
 
 # Upload the file to the container
-$fileName = Split-Path $LocalFilePath -Leaf
 Set-AzStorageBlobContent -File $LocalFilePath -Container $ContainerName -Blob $fileName -Context $ctx
 
 # Generate public URL for the file

67be61f54d4f7545dea9824b5c616812fbf10fe4
Vyente David Ruffin <76492205+david-ruffin@users.noreply.github.com>
2024-01-21 17:24:18 -0800

    Update GetAzureToken.ps1

--- functions/GetAzureToken.ps1
+++ functions/GetAzureToken.ps1
@@ -1,29 +1,35 @@
+<#
+.SYNOPSIS
+Acquires an Azure access token using a service principal.
+
+.DESCRIPTION
+This function makes a REST API call to Azure's OAuth 2.0 token endpoint to obtain an access token for Azure REST API calls.
+It's designed for non-interactive authentication using service principal credentials.
+
+.PARAMETER client_id
+The Client ID of the Azure service principal.
+
+.PARAMETER client_secret
+The Client Secret of the Azure service principal. It is handled as a SecureString for security.
+
+.PARAMETER tenant_id
+The Tenant ID of the Azure account.
+
+.EXAMPLE
+$accessToken = GetAzureToken -client_id 'xxxxxx' -client_secret 'xxxxxx' -tenant_id 'xxxxxx'
+
+.NOTES
+Modules required:
+- Az.Accounts: For using Azure-specific cmdlets (e.g., Get-AzAccessToken).
+- Microsoft.PowerShell.Utility: For making REST API calls using Invoke-RestMethod.
+Ensure these modules are installed and up to date in your PowerShell environment.
+Use 'Install-Module -Name Az -AllowClobber -Scope CurrentUser' to install the Az module.
+
+.LINK
+https://learn.microsoft.com/en-us/powershell/module/az.accounts/get-azaccesstoken
+#>
+
 function GetAzureToken {
-    <#
-    .SYNOPSIS
-    Acquires an Azure access token using a service principal.
-
-    .DESCRIPTION
-    This function makes a REST API call to Azure's OAuth 2.0 token endpoint to obtain an access token. 
-    It's designed to be used for non-interactive authentication to Azure for making API requests.
-
-    .PARAMETER client_id
-    The Client ID of the Azure service principal.
-
-    .PARAMETER client_secret
-    The Client Secret of the Azure service principal. It should be treated securely.
-
-    .PARAMETER tenant_id
-    The Tenant ID of the Azure account.
-
-    .EXAMPLE
-    $accessToken = GetAzureToken -client_id 'xxxxxx' -client_secret 'xxxxxx' -tenant_id 'xxxxxx'
-
-    .NOTES
-    This function is useful for scripts that need to authenticate with Azure non-interactively, 
-    such as in automated workflows or CI/CD pipelines.
-    #>
-
     param(
         [Parameter(Mandatory=$true)]
         [string]$client_id,

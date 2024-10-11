3d55f78e39b5abc721dc8462404484a42be68cac
Vyente Ruffin <76492205+david-ruffin@users.noreply.github.com>
2024-10-11 16:51:19 -0700

    Create run-arccmd.ps1

---
+++ Azure/arc/run-arccmd.ps1
@@ -0,0 +1,7 @@
+# Initiate a mars backup on arc enabled server
+$script = @"
+Import-Module -Name 'C:\Program Files\Microsoft Azure Recovery Services Agent\bin\Modules\MSOnlineBackup\MSOnlineBackup.psd1'
+Get-OBPolicy | Start-OBBackup
+"@
+
+New-AzConnectedMachineRunCommand -ResourceGroupName $resourceGroupName -Location "westus" -SourceScript $script -RunCommandName $patchName -MachineName $arcVMName
